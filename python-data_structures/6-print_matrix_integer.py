5d158eba004afbd634a4886ab1beaec2dfcfb582
Vozudo <jploperatobon@gmail.com>
2022-09-23 10:27:43 -0500

    [6 File]: print matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from turtle import pensize
+
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
+    else:
+        for x in range(0, len(matrix)):
+            for y in range(0, matrix[x]):
+                if y != len(matrix[x]) - 1:
+                    print(matrix[x][y], end=" ")
+                else:
+                    print(matrix[x][y])
