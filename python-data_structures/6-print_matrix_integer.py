7f36de13b479275be55bda0f4c4978fd8c4f7711
Vozudo <jploperatobon@gmail.com>
2022-09-23 10:36:04 -0500

    [6 File]: re do because of .format requirement

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
+    for x in matrix:
+        for v in range(len(x)):
+            if v < (len(x) - 1):
+                print("{:d} ".format(x[v]), end="")
+            else:
+                print("{:d}".format(x[v]), end="")
         print("")
-    else:
-        for x in range(0, len(matrix)):
-            for y in range(0, matrix[x]):
-                if y != len(matrix[x]) - 1:
-                    print(matrix[x][y], end=" ")
-                else:
-                    print(matrix[x][y])
