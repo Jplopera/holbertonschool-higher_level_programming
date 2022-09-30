880fce0fd9b31b68df8083db90594bcaa8f9ded6
Vozudo <jploperatobon@gmail.com>
2022-09-30 12:18:39 -0500

    [2 File]: cheker errors

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,10 +5,11 @@
 class Square:
     """empty class Square"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Instantiation with size"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
