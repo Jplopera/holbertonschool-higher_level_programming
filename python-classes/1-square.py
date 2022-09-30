009e05e182ce04d1531778eb7d1671ba0ccc153c
Vozudo <jploperatobon@gmail.com>
2022-09-30 12:05:56 -0500

    [1 File]: Instantiation with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define an empty class square"""
+
+
+class Square:
+    """empty class Square"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
