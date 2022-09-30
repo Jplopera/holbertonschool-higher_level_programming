1572017e7d60a5b75c3eb450bc0eae8756ed321a
Vozudo <jploperatobon@gmail.com>
2022-09-30 12:22:03 -0500

    [3 File]: that returns the current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define an empty class square"""
+
+
+class Square:
+    """empty class Square"""
+
+    def __init__(self, size=0):
+        """Instantiation with size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """instance method that returns the current square area"""
+        return (self.__size ** 2)
