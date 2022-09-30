fc1da8a41620bba81e20f2b892e08be496c862ee
Vozudo <jploperatobon@gmail.com>
2022-09-30 12:29:10 -0500

    [4 File]: Access and update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Define an empty class square"""
+
+
+class Square:
+    """empty class Square"""
+
+    def __init__(self, size=0):
+        """Instantiation with size"""
+        self.__size = size
+
+    def area(self):
+        """instance method that returns the current square area"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """etrieves the size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
