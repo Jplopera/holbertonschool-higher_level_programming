fcc2e51499292c5b71babcb27f197375be8ee807
Vozudo <jploperatobon@gmail.com>
2022-10-14 03:43:49 -0500

    [Task 3]

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns the JSON representation of an object"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns th JSON representation of my_obj"""
+    return json.dumps(my_obj)
