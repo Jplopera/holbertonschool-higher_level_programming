53554291e3557f1e179f9ac263f910627f5faac6
Vozudo <jploperatobon@gmail.com>
2022-10-14 04:06:14 -0500

    [Task 7]

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list
+""" Dcript that adds all arguments to a Python list
 and then save them to a file
 """
-
-
 import json
 import sys
 import os.path
-
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
