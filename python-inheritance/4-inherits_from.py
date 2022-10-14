70460afdd2e87995ee4550cda7ffc92567794a0f
Vozudo <jploperatobon@gmail.com>
2022-10-14 02:17:51 -0500

    [Task 4]

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""instance of a class cheking function"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False"""
+    return isinstance(obj, a_class) and type(obj) != a_class
