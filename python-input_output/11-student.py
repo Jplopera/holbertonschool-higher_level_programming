f1f0dbd416272144df4c3a4b03acb2d6a5483e71
Vozudo <jploperatobon@gmail.com>
2022-10-14 04:20:44 -0500

    [Task 9-11]

---
+++ python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Crates Student class"""
+
+
+class Student:
+    """define class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for x in json:
+            self.__dict__.update({x: json[x]})
