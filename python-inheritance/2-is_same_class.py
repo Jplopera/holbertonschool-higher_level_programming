47e4884cbb59199b4e07890000a8d85cebdccf86
Vozudo <jploperatobon@gmail.com>
2022-10-14 02:06:41 -0500

    [Task 2]

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class cheking function"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if the object is the specified class ; otherwise False"""
+
+    return type(obj) == a_class
