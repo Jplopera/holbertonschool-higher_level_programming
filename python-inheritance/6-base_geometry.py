9d8203e2ecb44c06ca74d50be06ebbd4367f267c
Vozudo <jploperatobon@gmail.com>
2022-10-14 02:22:35 -0500

    [Task 5-6]

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class"""
+
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
