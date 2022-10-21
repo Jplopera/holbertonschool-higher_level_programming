deff46ac1fbb65c1d812e3e83eec277c9347f5a2
Vozudo <jploperatobon@gmail.com>
2022-10-21 03:27:17 -0500

    [Task 15] Json dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module that contains "Base" class"""
+import json
 
 
 class Base:
@@ -13,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns a JSON string list of dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        return (json.dumps(list_dictionaries))
