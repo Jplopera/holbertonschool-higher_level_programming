815daeefa6cf804b47b6acbc0a4a22c1aa91fcf7
Vozudo <jploperatobon@gmail.com>
2022-10-21 03:38:49 -0500

    [Task 16] string to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,4 +20,18 @@ class Base:
         """Returns a JSON string list of dictionaries"""
         if list_dictionaries is None:
             return "[]"
-        return (json.dumps(list_dictionaries))
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON string of list_objs to a file"""
+        filename = "{}.json".format(cls.__name__)
+        list_dic = []
+
+        for i in range(len(list_objs)):
+            list_dic.append(list_objs[i].to_dictionary())
+
+        lists = cls.to_json_string(list_dic)
+
+        with open(filename, 'w') as f:
+            f.write(lists)
