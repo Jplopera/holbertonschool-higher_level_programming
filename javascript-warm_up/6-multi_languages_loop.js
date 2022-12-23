2f7442495549e61ce8e20c0747871e9d487165fb
Vozudo <jploperatobon@gmail.com>
2022-12-22 19:45:48 -0500

    Identation error

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,5 @@
 const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let y = 0; y < myVar.length; y++) {
-    console.log(myVar[y]);
+  console.log(myVar[y]);
 }
--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) === true) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let y = 0; y < process.argv[2]; y++) {
-        console.log('y'.repeat(process.argv[2]));
-    }
+  for (let y = 0; y < process.argv[2]; y++) {
+    console.log('y'.repeat(process.argv[2]));
+  }
 }
