fc0ac347ded800b8620b146696803d65b61aa879
Vozudo <jploperatobon@gmail.com>
2022-12-22 19:48:56 -0500

    Typo error

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,6 +3,6 @@ if (isNaN(process.argv[2]) === true) {
   console.log('Missing size');
 } else {
   for (let y = 0; y < process.argv[2]; y++) {
-    console.log('y'.repeat(process.argv[2]));
+    console.log('x'.repeat(process.argv[2]));
   }
 }
